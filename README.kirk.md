ef24ccd2639afb09deb3ad59cd563841f7e259e0
Yecheng Fu <cofyc.jackson@gmail.com>
2018-07-20 14:34:17 +0800

    Update kubelet.spec automatically.

--- README.kirk.md
+++ README.kirk.md
@@ -52,15 +52,5 @@ cd $GOPATH/src/k8s.io/release
 git checkout kirk-release
 cd rpm
 cp $GOPATH/src/k8s.io/kubernetes/_output/dockerized/bin/linux/amd64/kubelet .
-```
-
-#### update kubelet.spec and build
-
-编辑 rpm/kubelet.spec 文件，主要是：
-
-- 更新版本号 KUBE_VERSION`，注意需要将 - 变成 _ ，比如：`1.7.12-29+8320ccc842e9ac` -> `1.7.12_29+8320ccc842e9ac`
-- 更新 changelog
-
-```
 ./docker-build.sh amd64/x86_64
 ```
--- rpm/docker-build.sh
+++ rpm/docker-build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+# update kubelet.spec
+DATE=$(date +'%a %b %d %Y')
+VERSION=$(./kubelet --version | cut -d ' ' -f 2 | tr -s '-' '_')
+echo "DATE: $DATE"
+echo "VERSION: $VERSION"
+
+cat kubelet.spec.orig | sed "s/{{DATE}}/${DATE}/g" | sed "s/{{VERSION}}/${VERSION}/g" > kubelet.spec
+
+# build
+
 docker build -t kubelet-rpm-builder .
 echo "Cleaning output directory..."
 sudo rm -rf output/*
